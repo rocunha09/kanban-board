4219d0a66dfe175a86915bab518e81d9d15dedd6
Marcelo Amorim <marceloebed@hotmail.com>
2021-10-18 17:47:35 -0300

    Alterações na index
    Isolando css e js

---
+++ Script.js
@@ -0,0 +1,57 @@
+function moveForward(id){
+    var task = document.getElementById(id)
+    var arrow = document.getElementById(id)
+    var item = task
+    task.remove()
+    
+    var colunas = ['backlog', 'toDo', 'doing', 'done'];
+
+    var ColunaAtual = task.getAttribute('name')
+    ColunaAtual = ColunaAtual.split("_").pop()
+    
+    for(var i = 0; i < colunas.length; i++){
+        if (ColunaAtual == colunas[i]) {
+            var proxima = i+1
+            console.log(ColunaAtual + ' - ' +colunas[proxima]);
+            ColunaAtual  = colunas[proxima]
+            
+            var colunaDestino = colunas[proxima]
+            var destino = document.getElementById(colunaDestino)
+            
+            var novoName = id + '_' + colunaDestino
+            task.setAttribute('name', novoName)
+            
+            destino.appendChild(item);
+            return;
+        }
+    }
+}
+
+
+function moveBack(id){
+    var task = document.getElementById(id)
+    var item = task
+    task.remove()
+    
+    var colunas = ['backlog', 'toDo', 'doing', 'done'];
+
+    var ColunaAtual = task.getAttribute('name')
+    ColunaAtual = ColunaAtual.split("_").pop()
+    
+    for(var i = 0; i < colunas.length; i++){
+        if (ColunaAtual == colunas[i] && i>0) {
+             var proxima = i-1
+             console.log(ColunaAtual + ' - ' +colunas[proxima]);
+            ColunaAtual  = colunas[proxima]
+            
+            var colunaDestino = colunas[proxima]
+            var destino = document.getElementById(colunaDestino)
+           
+            var novoName = id + '_' + colunaDestino
+            task.setAttribute('name', novoName)
+            
+            destino.appendChild(item);
+            return;
+        }
+    }
+}
