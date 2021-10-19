43647a4d6b36f8c33aa12d3490620197e3bb5233
rocunha09 <ro.cunha00)@gmail.com>
2021-10-19 18:47:32 -0300

    realizado ajuste no script.js, simplificando função de movimento, iniciando leitura de dados através de um data.json

--- Script.js
+++ Script.js
@@ -1,66 +1,51 @@
-function moveForward(id){
+function movement(id, movement){
     var task = document.getElementById(id)
-    var arrow = document.getElementById(id)
     var item = task
     
     var colunas = ['backlog', 'toDo', 'doing', 'done'];
-    
     var ColunaAtual = task.getAttribute('name')
     ColunaAtual = ColunaAtual.split("_").pop()
-    if (ColunaAtual == 'done'){
-        return
-    }
+    
+    if (ColunaAtual == 'backlog' && movement == 'back'){
+        return;
+    } 
+
+    if (ColunaAtual == 'done' && movement == 'moveForward'){
+        return;
+    } 
+    
     task.remove()
     
     for(var i = 0; i < colunas.length; i++){
-       
+        
         if (ColunaAtual == colunas[i]) {
-            var proxima = i+1
-            console.log(ColunaAtual + ' - ' +colunas[proxima]);
-            ColunaAtual  = colunas[proxima]
-            
-            var colunaDestino = colunas[proxima]
-            var destino = document.getElementById(colunaDestino)
-            
-            var novoName = id + '_' + colunaDestino
-            task.setAttribute('name', novoName)
-            
-            destino.appendChild(item);
-            return;
-        }
-        
-    }
-}
-
-
-function moveBack(id){
-    var task = document.getElementById(id)
-    var item = task
-    
-    var colunas = ['backlog', 'toDo', 'doing', 'done'];
-    
-    var ColunaAtual = task.getAttribute('name')
-    ColunaAtual = ColunaAtual.split("_").pop()
-    if (ColunaAtual == 'backlog'){
-        return
-    }
-    task.remove()
-    
-    for(var i = 0; i < colunas.length; i++){
-        
-        if (ColunaAtual == colunas[i] && i>0) {
-             var proxima = i-1
-             console.log(ColunaAtual + ' - ' +colunas[proxima]);
+            var proxima = movement == 'moveForward' ? i+1 : i-1
             ColunaAtual  = colunas[proxima]
             
             var colunaDestino = colunas[proxima]
             var destino = document.getElementById(colunaDestino)
            
             var novoName = id + '_' + colunaDestino
-            task.setAttribute('name', novoName)
+            item.setAttribute('name', novoName)
             
             destino.appendChild(item);
             return;
         }
     }
 }
+
+//criar uma lista de tasks para popular o kanban usando data.json
+//necessário parametrizar cors
+
+var ajax = new XMLHttpRequest()
+ajax.open("GET", "data.json")
+ajax.responseType = "json"
+
+ajax.send();
+
+ajax.addEventListener("readystatechange", function() {
+    if(ajax.readyState === 4 && ajax.status === 200){
+        console.log(ajax)
+        console.log(ajax.response)
+    }
+})
